1a62ad3c94a85e40da25c8b388ff612c96413492
sigitirawan99 <sigitirawan939@gmail.com>
2026-02-03 07:55:18 +0700

    fix

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,14 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import { getUser } from "@/lib/supabase/auth";
+import { useRouter } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { push } = useRouter();
+  const user = getUser();
+  if (!user) {
+    push("/sign-in");
+  }
   return (
     <SidebarProvider>
       <AppSidebar />
